3bc2f9922d64b1261f218923aeed022262b6fd80
Anton Autushka <a.autushka@aetsoft.by>
2017-12-07 17:57:04 +0300

    Simple echo server

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net"
+    "log"
+)
 
 func main() {
-    fmt.Printf("hello world\n")
+    endpoint := "localhost:7777"
+    l, err := net.Listen("tcp", endpoint)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer l.Close()
+
+    log.Print("Listening incoming connections on ", endpoint)
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        log.Print("Connection accepted from ", conn.RemoteAddr())
+        go handle_connection(conn)
+    }
+}
+
+func handle_connection(conn net.Conn) {
+    defer log.Print("Connection closed ", conn.RemoteAddr())
+    defer conn.Close()
+
+    buf := make([]byte, 1024)
+
+    for {
+        bytesRead, err := conn.Read(buf)
+        if err != nil{
+            break
+        }
+        fmt.Println("Read some bytes")
+        conn.Write(buf[0: bytesRead])
+    }
 }
